4dc90972a578d80d2326f7accbed5c08a498d062
Domagoj Halusek <dohaluse@vsite.hr>
2021-10-18 12:34:08 +0200

    Remove bin i obj folders

--- TechMania_Client/obj/Release/net5.0/browser-wasm/PubTmp/Out/wwwroot/js/StripePayment.js
+++
@@ -1,6 +0,0 @@
-﻿redirectToCheckout = function (sessionId) {
-    var stripe = Stripe('pk_test_51JaozTFd6I9HEF2Kg6DHDmFr6F2qUTGYayPJq3CSRjNRWrvmuuQ3JhTbTEBKeCHie4DDgyRQyPWxbBOz5taBqDTh00QQUc357b');
-    stripe.redirectToCheckout({
-        sessionId: sessionId
-    });
-};
